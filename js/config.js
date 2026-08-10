e75ba48a7d0b3ba29dbbdffb6dd229a28ebc1ba6
Hector0420 <106408698+Hector0420@users.noreply.github.com>
2026-08-10 17:08:40 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,3 +2,5 @@
 const SUPABASE_URL = 'https://gxuiqmhmqdhaciagfoqf.supabase.co/rest/v1/';
 const SUPABASE_ANON_KEY ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4dWlxbWhtcWRoYWNpYWdmb3FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMzE4NTIsImV4cCI6MjEwMTcwNzg1Mn0.3lbe0mX_FKqgq2uN-XON_yTtRnh7c7Z9TpznpJu74OM';
 const GA_MEASUREMENT_ID = 'G-XXXXXXXX'; // Opcional, déjalo vacío si no usas// JavaScript Document
+
+// sb_publishable_DilEFv9hpZuVaKeKw4iUlg_UEZAtlhA
